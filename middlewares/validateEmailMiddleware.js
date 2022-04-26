a9fbcc6dfe267c030f9a9c361fb97fc80b69cca3
camila <camilaczeg@gmail.com>
2022-04-26 01:21:16 -0300

    requisito 4: valida formato do email

--- middlewares/validateEmailMiddleware.js
+++ middlewares/validateEmailMiddleware.js
@@ -2,10 +2,19 @@ const { BAD_REQUEST } = require('../statusCode');
 
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  const validFormat = email.toLowerCase().match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
-  if (!email || !validFormat) {
-    return res.status(BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+  if (!email) {
+    return res
+      .status(BAD_REQUEST)
+      .json({ message: 'O campo "email" é obrigatório' });
   }
+
+  const validFormat = email.toLowerCase().match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+  if (!validFormat) {
+    return res
+      .status(BAD_REQUEST)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
   next();
 };
 
